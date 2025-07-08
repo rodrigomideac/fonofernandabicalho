f7dc86789b0aec4080277beca834edd8537f4b5f
rodrigo <rodrigomidea@gmail.com>
2025-07-07 23:58:32 -0300

    Release

---
+++ _next/static/kJNJpwbj8nyv_jv6y3SwL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/359-14858fb9e84172b6.js","static/css/5036c5365c87c0b7.css","static/chunks/pages/index-177bc5c68044cb79.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
