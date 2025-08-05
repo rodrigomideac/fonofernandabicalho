5c58a68201be79e577a7af2c2af640feff9cb66f
Rodrigo Midea <rodrigomidea@gmail.com>
2025-08-05 17:09:11 -0300

    Release

---
+++ _next/static/L8_hNXZczi3Jn94G253zM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/359-31ecff41c155c68e.js","static/css/ed574fa2e5291195.css","static/chunks/pages/index-f5d0ad56eec5ab82.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
